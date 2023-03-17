d9fd20d4d7223b242f873bfc52f68471aa6a651a
JhowAndrade <johanny_andrade@yahoo.com.br>
2023-03-16 22:09:43 -0300

    Desafio finalizadoD

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,12 +5,8 @@ export default function Footer() {
     return (
 
         <div className="cdt-footer">
-            <div>
                 <a href="#">@dscarrostop</a>
-            </div>
-            <div>
                 <p>Rua da Pipoca, 150</p>
-            </div>
         </div>
 
     );
